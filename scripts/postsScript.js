abd8d7eedfcd5ed008540b88d4505d5b26589f10
Ben Covey <benvcovey@gmail.com>
2017-03-29 18:00:40 -0300

    Added url variable

--- scripts/postsScript.js
+++ scripts/postsScript.js
@@ -1,6 +1,22 @@
 var PostID;
 //we will also need a function to re-display the original post 
 //that all these comments are about
+var url = getQueryVariable("id");
+
+function getQueryVariable(variable) {
+    //debugger;
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            getPost(pair[1])
+            return pair[1];
+        }
+    } 
+
+  console.log('Query Variable ' + variable + ' not found');
+}
 function getPost(postID) {
     //set global variable that way we dont have to 
     //send the same value into more functions
