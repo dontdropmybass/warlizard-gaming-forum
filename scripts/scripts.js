a2e985f3721a3a0c6449b856781e9f05b8dbd369
Alexander Cochrane <acochrane@almightycorp.com>
2017-03-29 18:09:04 -0300

    changed script.js to use promises instead of ajax success and failure

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,11 +4,11 @@ var myroot = serverroot;
 var posts;
 
 function getPosts() {
-
-    $.ajax({
-        url: myroot + "/posts",
-        method: 'GET',
-        success:function(data) {
+    $.when(
+        $.ajax({
+            url: myroot
+        }))
+        .done(function(data) {
             console.log(data);
             posts = data;
             $("#posts").html("");
@@ -17,61 +17,59 @@ function getPosts() {
                 //console.log(data[i]['title']);
                 //console.log(data[i]['body']);
                 $("#posts").append( "<div id='post" + i + "' class='card'>" +
-                                    "<div id='post" + i + "title' class='card-title' >" +
-                                    "<h3>" + data[i]['title'] + "</h3>" + "</div>" +
-                                    (data[i].imgLink!=undefined&&data[i].imgLink!=""?
-                                        "<img src='"+data[i].imgLink+"' class='materialboxed'/>":
-                                        "")+
-                                    "<div id='post" + i + "body' class='card-body'>" +
-                                    "<p>" + data[i]['body'] + "</p>" +  "</div>" +
-                                    "<div id=footer class=card-footer><a href='/post.html?id="+i +"'>Add a Comment</a></div></div>");
+                    "<div id='post" + i + "title' class='card-title' >" +
+                    "<h3>" + data[i]['title'] + "</h3>" + "</div>" +
+                    (data[i].imgLink!=undefined&&data[i].imgLink!=""?
+                        "<img src='"+data[i].imgLink+"' class='materialboxed'/>":
+                        "")+
+                    "<div id='post" + i + "body' class='card-body'>" +
+                    "<p>" + data[i]['body'] + "</p>" +  "</div>" +
+                    "<div id=footer class=card-footer><a href='/post.html?id="+i +"'>Add a Comment</a></div></div>");
             }
-
-        },
-        error: function (e) {
+        })
+        .fail(function(e) {
             console.log(e);
-        }
-    });
-
+        });
 }
 
 function makePost(){
     debugger;
-  //regex for imgur link
-  //http://stackoverflow.com/questions/26457485/how-would-i-detect-what-is-an-imgur-picture-link-and-what-isnt
-  var url = $("#add-img").val();
-  //nevermind this dosen't work
-  var imgur_re = new RegExp ("/^https?:\/\/(\w+\.)?imgur.com\/(\w*\d\w*)+(\.[a-zA-Z]{3})?$");
-  if(url != "" && url != ""){
-  var res = imgur_re.test(url);
-}
-else{
-  //if they didn't enter a link then it's chill
-  //no need to validate
-  res = true;
-}
-  if(res){
-    $.ajax({
-        url: myroot + '/posts',
-        type: 'POST',
-        data: { 
-            title: $("#add-title").val(),
-            body: $("#add-body").val(),
-            imgLink: url
-        },
-        dataType: 'json',
-    error: function (request, status, error) {
-        alert(request.responseText);
-    },
-    success:function(){
-        location.reload(true);
+    //regex for imgur link
+    //http://stackoverflow.com/questions/26457485/how-would-i-detect-what-is-an-imgur-picture-link-and-what-isnt
+    var url = $("#add-img").val();
+    var res = true;
+    //nevermind this dosen't work
+    var imgur_re = new RegExp ("/^https?:\/\/(\w+\.)?imgur.com\/(\w*\d\w*)+(\.[a-zA-Z]{3})?$");
+    if(url != "" && url != ""){
+        res = imgur_re.test(url);
     }
-
-  });
-}
-else{
-  //user entered improper URL so give them a message or something
-  }
+    else{
+        //if they didn't enter a link then it's chill
+        //no need to validate
+        res = true;
+    }
+    if(res){
+        $.when(
+            $.ajax({
+                url: myroot + '/posts',
+                type: 'POST',
+                data: {
+                    title: $("#add-title").val(),
+                    body: $("#add-body").val(),
+                    imgLink: url
+                },
+                dataType: 'json'
+            }))
+            .done(function(){
+                location.reload(true);
+            })
+            .fail(function (request, status, error) {
+                alert(request.responseText);
+            });
+    }
+    else{
+        //user entered improper URL so give them a message or something
+    }
 
 
 }
